b58d063cc47fc3781dbf080e5b85c091693b4289
RogerVDW_VS2019 <rogerscode@gmail.com>
2020-01-12 23:05:40 +0100

    Comments, console output, cleanup

--- InfiPuzzle2019_solution3/Source.cpp
+++ InfiPuzzle2019_solution3/Source.cpp
@@ -5,11 +5,13 @@
 #include <string>
 #include <iostream>
 
+// DEEL 1 VAN DE PUZZEL
+// =================================================================================================================================
 void Infi1()
 {	// aoc.infi.nl 2019, deel 1
 	// input
-	std::vector<std::pair<int, int>> flats = { {3,7},{5,9},{8,5},{10,6},{14,7},{16,10},{21,9},{25,10},{30,6},{33,8},{34,9},{37,10},{42,8},{45,5},{47,6},{49,9},{50,10},{54,8},{57,9},{58,10},{61,8} };
-	std::vector < std::pair<int, int>> sprongen = { {1,2},{2,0},{1,1},{3,1},{1,3},{4,0},{3,1},{4,0},{2,2},{0,1},{2,1},{4,0},{2,0},{1,1},{0,0},{0,1},{3,0},{2,1},{0,1},{2,0} };
+	std::vector<std::pair<int, int>> flats = { {2,5},{5,6},{8,8},{13,4},{17,5},{21,5},{24,7},{26,8},{27,9},{32,3},{37,3},{39,4},{43,2},{46,3},{47,2},{50,3},{53,5},{57,6},{59,3},{60,6} };
+	std::vector < std::pair<int, int>> sprongen = { {2,1},{2,2},{4,0},{3,1},{0,0},{2,2},{1,1},{0,1},{4,0},{4,0},{1,1},{3,0},{2,1},{0,0},{2,1},{2,2},{3,1},{1,0},{0,3} };
 
 	// Maak map van alle hoogtes van de flats: KEY= positie x van flat, VALUE = hoogte
 	std::map<int, int> hoogtes;
@@ -20,6 +22,8 @@ void Infi1()
 	int nSprongen = 0;
 
 	// Main loop: voer sprong uit tot [vallen] of tot [successvol landen na laatste sprong]
+	std::cout << "Santa begint met springen, hou je hart vast:";
+	std::cout << "\n== START ===\n";
 	for (int i = 0; i < sprongen.size(); i++)
 	{
 		std::cout << "Positie: (" << pos.first << "," << pos.second << "). ";
@@ -37,10 +41,13 @@ void Infi1()
 
 	// Resultaat tonen
 	if (nSprongen == sprongen.size()) std::cout << "success, nSprongen = " << nSprongen; else std::cout << "Fail na sprong: " << nSprongen << ", de kerstman valt dus bij sprong " << nSprongen + 1;
+	std::cout << "\n== END ===\n";
 	std::cout << "\nDruk [Enter] om door te gaan..";
 	std::cin.get();
 }
 
+// DEEL 2 VAN DE PUZZEL
+// =================================================================================================================================
 bool kanVooruitLanden(const std::pair<int, int> startPos, const std::pair<int, int> sprong, const std::map<int, int>& hoogtes)
 {
 	// Test of Santa vanaf startpositie {x,y} op een nieuw dak kan landen voor een gegeven sprong {sprong_x,sprong_y}
@@ -54,6 +61,8 @@ bool kanVooruitLanden(const std::pair<int, int> startPos, const std::pair<int, i
 }
 std::string Hash(std::pair<int, int> pos)
 {
+	// Deze hash wordt gebruikt om unieke landingsposities te vertalen naar een string die zal fungeren als unieke 
+	// naam van de Nodes
 	std::string posName;
 	posName.append("(");
 	posName.append(std::to_string(pos.first));
@@ -63,26 +72,27 @@ std::string Hash(std::pair<int, int> pos)
 	return posName;
 }
 struct Arc; // Forward declaration nodig want Node en Arc zijn gekoppeld
-struct Node // Deze datastructuur vormt de basis voor onze graph (zie bestaat uit nodes en arcs)
+struct Node 
 {
+	// Deze datastructuur vormt de basis voor onze graph (zie bestaat uit nodes en arcs). In dit voorbeeld zijn de
+	//Nodes de dakposities
 	Node(std::pair<int, int> p)
 		:
-		x(p.first),
-		y(p.second),
-		name(Hash(p))
+		name(Hash(p)),
+		position(p)
 	{}
 	// Basisvelden t.b.v. graph functionaliteit
 	std::string name;
-	int x;
-	int y;
+	std::pair<int,int> position;
 	std::set<Arc*> arcs;
 	friend bool operator==(const Node& p1, const Node& p2)
 	{
 		return (p1.name == p2.name);
 	}
 };
-struct Arc
+struct Arc 
 {
+	// Deze datastructuur definieert de arcs (verbindingen tussen de nodes, dus in dit voorbeeld: de sprongen)
 	Arc(Node* s, Node* f, int c)
 		:
 		start(s),
@@ -94,9 +104,9 @@ struct Arc
 	Node* finish;
 	int cost;
 };
-
-int GetPathCost(const std::vector<Arc*>& path)
+int KostenVanPad(const std::vector<Arc*>& path)
 {
+	// Itereert over alle arcs (sprongen van dak-tot-dak in het pad) en telt de energie op van iedere sprong
 	int cost = 0;
 	for (Arc* arc : path)
 	{
@@ -104,16 +114,19 @@ int GetPathCost(const std::vector<Arc*>& path)
 	}
 	return cost;
 }
-
-struct GreaterPathLength
+struct GroterePadKosten
 {
+	// Dit is een compare function die gebruikt wordt voor het sorteren (prioriteren) in de priority_map class voor de toepassing 
+	// in het Dijkstra algorithme
 	bool operator()(const std::vector<Arc*>& lhs, const std::vector<Arc*>& rhs) const
 	{
-		return GetPathCost(lhs) > GetPathCost(rhs);
+		return KostenVanPad(lhs) > KostenVanPad(rhs);
 	}
 };
-bool NodeHasArcToPosition(Node* node, std::string landingNodeName)
+bool NodeBevatArcNaarPositie(Node* node, std::string landingNodeName)
 {
+	// Deze functie test of een sprong al is toegevoegd aan een gegeven positie; nodig om te voorkomen dat meerdere sprongen worden 
+	// geregistreerd (we willen alleen de kleinste sprong)
 	for (auto a : node->arcs)
 	{
 		if (a->finish->name == landingNodeName)
@@ -123,13 +136,16 @@ bool NodeHasArcToPosition(Node* node, std::string landingNodeName)
 	}
 	return false;
 }
-
-void FillWithAllPossibleArcs(Node* start, std::map<int, int>& hoogtes)
+void VindAlleBereikbareSchoorstenen(Node* start, std::map<int, int>& hoogtes)
 {
-	std::vector < std::pair<int, int>> basisSprongen = { {0,0},{0,1},{0,2 },{0,3},{0,4},{1,0},{1,1},{1,2},{1,3},{2,0},{2,1},{2,2},{3,0},{3,1},{4,0} };
-	for (auto s : basisSprongen)
+	// Voor een gegeven positie (start node) zoekt deze functie alle mogelijke nieuwe landingsposities (schoorstenen), en de 
+	// minimale sprongen die hiervoor genomen moeten worden.
+	// Deze nieuwe posities en bijbehorende sprongen worden toegevoegd aan de graph als respectievelijk nieuwe Node* en Arc*
+	// De vector basisSprongen van alle mogelijke sprongen is gesorteerd van lage energie naar hoge energie, dit helpt om optimale sprongen te vinden
+	std::vector < std::pair<int, int>> basisSprongen = { {0,0},{0,1},{0,2},{0,3},{0,4},{1,0},{1,1},{1,2},{1,3},{2,0},{2,1},{2,2},{3,0},{3,1},{4,0} };
+	for (auto s : basisSprongen) // loop door alle basissprongen
 	{
-		std::pair<int, int> currentPos = { start->x,start->y };
+		std::pair<int, int> currentPos = { start->position.first,start->position.second };
 		if (kanVooruitLanden(currentPos, s, hoogtes))
 		{
 			// creeer nieuwe landingspositie
@@ -137,9 +153,11 @@ void FillWithAllPossibleArcs(Node* start, std::map<int, int>& hoogtes)
 			landingPos.first = currentPos.first + s.first + 1;
 			landingPos.second = hoogtes.find(landingPos.first)->second;
 			int energy = s.first + s.second;
-			// als de landingspositie nog niet in de graph zit, voeg deze toe aan de queue
 			std::string landingNodeName = Hash(landingPos);
-			if (!NodeHasArcToPosition(start, landingNodeName))
+			// Als de landingspositie nog niet in de graph zit, voeg deze dan toe aan de graph die we aan het construeren zijn
+			// zo niet, dan negeren we de sprong (we weten al dat de eerdere geregistreerde sprong een lagere energie heeft, omdat
+			// de vector basisSprongen gesorteerd is naar toenemende energie)
+			if (!NodeBevatArcNaarPositie(start, landingNodeName))
 			{
 				Node* newNode = new Node(landingPos);
 				Arc* newArc = new Arc(start, newNode, energy);
@@ -150,90 +168,99 @@ void FillWithAllPossibleArcs(Node* start, std::map<int, int>& hoogtes)
 }
 std::vector<Arc*> VindKortstePad(Node* start, Node* finish, std::map<int, int>& hoogtes)
 {
+	// Dit is de hoofdfunctie van de oplossing die een Dijkstra Shortest Path methode implementeert met drie hulp datastructuren:
+	// ==> Een vector die opgebouwd wordt met de kortste paden, totdat het einddoel bereikt wordt
 	std::vector<Arc*> path;
-	std::priority_queue< std::vector<Arc*>, std::vector<std::vector<Arc*>>, GreaterPathLength> queue;
-	//std::priority_queue<Arc*,std::vector<Arc*>, GreaterPathLength> queue;
+	// ==> Een priority queue die zorgt dat het algoritme "greedy" wordt (het briljante inzicht van Dijkstra)
+	std::priority_queue< std::vector<Arc*>, std::vector<std::vector<Arc*>>, GroterePadKosten> queue;
+	// ==> Een map die bijhoudt welke Nodes als "gefixt" zijn, d.w.z.: waarvan we gaandeweg het algo te weten komen wat de kortste weg hiernaartoe is
 	std::map<std::string, int> fixed;
-	while (start != finish)
+	
+	// Main loop
+	// We verplaatsten de startpositie sprong per sprong totdat we de gezochte schoorsteen bereiken
+	while (start->name != finish->name)
 	{
-		if (fixed.find(start->name) == fixed.end()) // als het startpunt niet eerder is vastgepind, dan weten we dat deze nu de kortste route is
+		if (fixed.find(start->name) == fixed.end()) // als het startpunt niet eerder is vastgepind, dan weten we dat deze per definitie de kortste route is
 		{
-			fixed[start->name] = GetPathCost(path);
-			FillWithAllPossibleArcs(start, hoogtes);
+			fixed.insert({ start->name,KostenVanPad(path) });
+			VindAlleBereikbareSchoorstenen(start, hoogtes); // Zoek nu alle bereikbare nieuwe Nodes vanaf de huidige positie en voeg die Arcs toe aan start
 			for (Arc* a : start->arcs)
 			{
-				if (fixed.find(a->finish->name) == fixed.end())
+				//if (fixed.find(a->finish->name) == fixed.end()) // Als we de gevonden nieuwe positie niet eerder gevonden hebben (met een korter pad), voeg dan toe aan de queue
 				{
 					path.push_back(a);
 					queue.push(path);
-					path.erase(path.end() - 1); // CHECK
+					path.erase(path.end() - 1);
 				}
 			}
 		}
 		if (queue.empty())
 		{
-			//path.clear();
+			// Als gedurende het algoritme de queue leeg raakt, betekent dat dat er geen verbinding is van start naar finish, we retourneren dan een lege vector
+			path.clear();
 			return path;
 		}
 		path = queue.top(); queue.pop();
-		start = path[path.size() - 1]->finish;
+		start = path.back()->finish;
 	}
+	// Presenteer interessante vastgelegde informatie
+	std::cout << "Gedurende uitvoering van het Dijkstra algoritme is vastgesteld \nmet welke minimale energie de volgende punten kunnnen worden bereikt:\n";
+	std::cout << "\n== START ===\n";
+	for (auto f : fixed)
+	{
+		std::cout << f.first << ": " << f.second <<'\n';
+	}
+	std::cout << "== END ===\n";
+	std::cout << "\nDruk [Enter] om door te gaan..";
 	return path;
 }
-
 void Infi2()
 {
 	// aoc.infi.nl 2019, deel 2
 	// Input (kan json load doen, maar dit was even sneller en is triviaal)
-	//std::vector<std::pair<int, int>> flats = { {3,3},{5,5},{9,6},{13,7},{15,9},{16,10},{18,9},{21,10},{23,3},{25,4},{28,5},{29,7},{30,8},{34,7},{37,9},{39,7},{41,8},{42,10},{43,9},{47,10},{48,10},{50,6},{52,7},{53,10},{56,6},{57,4},{58,6},{59,2},{62,2} };
-	std::vector<std::pair<int, int>> flats = { {3,7},{5,9},{8,5},{10,6},{14,7},{16,10},{21,9},{25,10},{30,6},{33,8},{34,9},{37,10},{42,8},{45,5},{47,6},{49,9},{50,10},{54,8},{57,9},{58,10},{61,8} };
+	std::vector<std::pair<int, int>> flats = { {2,5},{5,6},{8,8},{13,4},{17,5},{21,5},{24,7},{26,8},{27,9},{32,3},{37,3},{39,4},{43,2},{46,3},{47,2},{50,3},{53,5},{57,6},{59,3},{60,6} };
 	// Maak map van alle hoogtes van de flats: KEY= positie x van flat, VALUE = hoogte
 	std::map<int, int> hoogtes;
 	for (auto p : flats) hoogtes[p.first] = p.second;
+
+	// Definieer start en eind Nodes (schoorstenen)
 	Node* start = new Node(flats.front());
 	Node* finish = new Node(flats.back());
-	Node* test = new Node({ 58, 10 });
-	FillWithAllPossibleArcs(test, hoogtes);
+
+	// Pas Dijkstra algoritme toe
 	auto kortstePad = VindKortstePad(start, finish, hoogtes);
-	int minEnergy = GetPathCost(kortstePad);
+	int minEnergie = KostenVanPad(kortstePad);
 
 	// Presenteer resultaten
-	//std::cout << "\nShortest path with Dijkstra: " << SimpleGraph::getPathCost(shortestPath)<<std::endl;
-	//std::cout << "\nOptimale route:\n";
-	//std::cout << "\n== START ===\n";
-	//for (auto a : shortestPath)
-	//{
-	//	std::cout << a->start->name << " -cost[" << a->cost << "]->" << a->finish->name << std::endl;
-	//}
-	//std::cout << "== END ===\n";
-
-	//graph.BFS(startNode);
-	//std::cout << "nNodes: " << graph.nNodes;
-
-
-
-
+	std::cout << "\nMinimale Energie om van "<<start->name<<" naar "<<finish->name<<" te komen: " << minEnergie << '\n' ;
+	std::cout << "\nOptimale route:\n";
+	std::cout << "\n== START ===\n";
+	for (auto a : kortstePad)
+	{
+		std::cout << a->start->name << " -cost[" << a->cost << "]->" << a->finish->name << '\n' ;
+	}
+	std::cout << "== END ===\n";
 	std::cout << "\nDruk [Enter] om te sluiten..";
 	std::cin.get();
 }
 
 int main()
 {
-	// https://aoc.infi.nl 2019
+	// https://aoc.infi.nl 2019 / Oplossing #3
 	// Deze oplossing maakt gebruik Dijkstra's shortest path algorithm, mooi om onze Nederlandse trots toe te passen. 
 	// 
-	// Naast de C++ Standard Library (STL) includes voor standaard container classes heb ik eigen "SimpleGraph" class 
-	// gebouwd en included.
+	// In tegenstelling tot oplossingen #1 en #2 is nu een aanpak geimplementeerd waarin niet eerste de volledige graph
+	// van alle mogelijke routes wordt gemapt. Dit is het briljante van Dijkstra: je blijft gedurende het algo steeds 
+	// de kortste uitbreiding van de route verkennen totdat je je doel vindt: dit is dan per definitie via de korste weg. 
 	//
-	// Basisgedachte: Vertaal de sprongen in een Directed Graph:
-	// > iedere positie is een Vertex (/Node)
-	// > iedere mogelijke sprong is een Edge (/Arc) naar een nieuwe positie
-	// > de kosten van iedere sprongen is een dataveld van de Edge
+	// Ik gebruik alleen C++ STL data structuren. Een paar interessante features (zoals de std::priority_queue<...>) worden
+	// gebruikt. Commentaar in de code spreekt hopelijk voor zich
 	//
-	// Als de graph eenmaal goed geconstrueerd is, is het eenvoudig om Dijkstra toe te passen.
+	// Nogmaals complimenten voor de puzzel, het was voor mij een perfecte manier om deze materie van allerlei kanten te
+	// benaderen.
 	// groeten! Rogier (rogerscode@gmail.com)
 
-	//Infi1(); // deel 1 van de puzzel
+	Infi1(); // deel 1 van de puzzel
 	Infi2(); // deel 2
 	return 0;
 }
